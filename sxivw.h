1fae06b494069050fc249d39371db3eaac5a2f9b
Bhavesh164 <bhaveshverma333@gmail.com>
2022-12-15 11:37:54 +0530

    update sxiv comments in sxiv wallpaper script
    update sxiv comments in sxiv wallpaper script

--- sxivw.h
+++ sxivw.h
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-#set wallpaper using sxiv
+# set wallpaper using sxiv 
 sxiv -r -q -o * | xargs -I {} gsettings set org.gnome.desktop.background picture-uri "$PWD/{}"
+# to set wallpaper press the m key
