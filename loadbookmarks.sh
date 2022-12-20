f1a26fe218a777be0692a01ed803cfc06ee6a77f
Bhavesh164 <bhaveshverma333@gmail.com>
2022-12-20 15:50:20 +0530

    simple bookmarks scripts
    simple bookmarks scripts

---
+++ loadbookmarks.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+file="$HOME/programs/bookmarks/snippet.txt"
+bookmark=$(awk '{print i++ " " $0}' "$file" | dmenu -p "select bookmark:" -i -l 25 | cut -d " " -f2)
+regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]+'
+# Use a regular expression to check if the variable is a URL
+if [[ $bookmark =~ $regex ]]; then
+	# The variable is a URL
+	google-chrome --new-tab "$bookmark"
+elif [ -z $bookmark ]; then
+  	notify-send "Please select a url"
+else
+  	# The variable is not a URL
+  	notify-send "$bookmark is not a URL"
+fi
---
+++ savebookmarks.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash 
+bookmark="$(xclip -o)"
+file="$HOME/programs/bookmarks/snippet.txt"
+if grep -q "^$bookmark$" "$file"; then
+	notify-send "Oops. " "Already bookmarked!"
+else 
+	regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]+'
+	if [[ $bookmark =~ $regex ]]; then
+		notify-send "bookmark added" "$bookmark saved to a file"
+		echo "$bookmark" >> "$file"
+	else 
+		notify-send "Not a valid url" 
+fi
