2ff87a0442d01679c6435e0119ea7111381fba8b
Bhavesh164 <bhaveshverma333@gmail.com>
2022-12-20 18:42:16 +0530

    remove echo statement
    remove echo statement

--- delbookmarks.sh
+++ delbookmarks.sh
@@ -5,7 +5,6 @@ SEARCH_STRING=$(awk '{print i++ " " $0}' "$file" | dmenu -p "select bookmark:" -
 # Extract the line number from the output of grep
 LINE_NUMBER=$(grep -n "$SEARCH_STRING" "$file" | awk -F: '{print $1}')
 if [[ -n $LINE_NUMBER ]] && [[ "$LINE_NUMBER" == ?(-)+([[:digit:]]) ]]; then  
-	echo '"$LINE_NUMBER"d'
 	sed -i -e "${LINE_NUMBER}d" "$file" 
 else 
 	notify-send "Please select URL"
