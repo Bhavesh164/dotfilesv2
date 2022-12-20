7492601ce98745cfe26df710a6b446b423cacc87
Bhavesh164 <bhaveshverma333@gmail.com>
2022-12-20 18:20:03 +0530

    delete bookmarks
    delete bookmarks

---
+++ delbookmarks.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+file="$HOME/programs/bookmarks/snippet.txt"
+# Prompt the user for the search string using dmenu
+SEARCH_STRING=$(awk '{print i++ " " $0}' "$file" | dmenu -p "select bookmark:" -i -l 25 | cut -d " " -f2)
+# Extract the line number from the output of grep
+LINE_NUMBER=$(grep -n "$SEARCH_STRING" "$file" | awk -F: '{print $1}')
+if [[ -n $LINE_NUMBER ]] && [[ "$LINE_NUMBER" == ?(-)+([[:digit:]]) ]]; then  
+	echo '"$LINE_NUMBER"d'
+	sed -i -e "${LINE_NUMBER}d" "$file" 
+else 
+	notify-send "Please select URL"
+fi
