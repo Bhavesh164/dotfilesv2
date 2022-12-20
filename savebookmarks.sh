015f4c124af28f9c54ea1e9623e3e4a4aeea3349
Bhavesh164 <bhaveshverma333@gmail.com>
2022-12-20 16:14:35 +0530

    bug fixes

--- savebookmarks.sh
+++ savebookmarks.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash 
-bookmark="$(xclip -o)"
+# bookmark="$(xclip -o)"
+bookmark="$(xclip -selection clipboard -o)"
 file="$HOME/programs/bookmarks/snippet.txt"
 if grep -q "^$bookmark$" "$file"; then
 	notify-send "Oops. " "Already bookmarked!"
