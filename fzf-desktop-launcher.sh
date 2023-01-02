a4ec6e6e3de664cd43011dfa0425993a627c782a
Bhavesh164 <bhaveshverma333@gmail.com>
2023-01-02 15:54:18 +0530

    for alacritty
    for alacritty

--- fzf-desktop-launcher.sh
+++ fzf-desktop-launcher.sh
@@ -9,13 +9,14 @@ exec=$(grep -oP "^Exec=\K.*" "/usr/share/applications/$programs.desktop") # he "
 # to use it in sxhkd
 # ctrl + alt + p
 # xterm -e "bash -c '/usr/local/bin/fzf-desktop-launcher.sh; exec bash'"
+# alacritty -e '/usr/local/bin/fzf-desktop-launcher.sh'
+
 
 # Launch the selected program
 nohup $exec &
 
 # close xterm
 pkill xterm
-
 # The command "xterm -e" is used to open an xterm terminal window and run a command in it. The command that is run in the xterm window is specified after the "-e" flag.
 
 # In this case, the command being run in the xterm window is:
@@ -29,8 +30,3 @@ pkill xterm
 # The "exec" command is used to replace the current shell process with a new process. In this case, it is used to start a new instance of Bash after the "fzf-desktop-launcher.sh" script has been run. This allows you to continue using the terminal after the script has finished running.
 #
 # Overall, this command opens an xterm terminal window and runs the "fzf-desktop-launcher.sh" script, which allows you to search and launch applications from the desktop. It then starts a new instance of Bash, so that you can continue using the terminal.
-
-
-
-
-
