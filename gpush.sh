6f8772b7a216ae221f4177499255c8118a10bd1e
Bhavesh164 <bhaveshverma333@gmail.com>
2023-01-06 14:24:14 +0530

    git push
    git push

---
+++ gpush.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# add all modified and deleted files
+git add -A
+
+# commit changes with a message passed as a command line argument
+git commit -m "$@"
+
+# check if the "origin" remote is configured
+if git remote | grep -q "origin"; then
+  # push changes to the "origin" remote
+  git push
+else
+  # display an error message
+  echo "Error: No remote 'origin' configured."
+fi
